05a25434236b0547477ae56af834d9a65d276ac4
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-10 02:01:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+function show_magicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician_1 = magicians_1[_i];
+        console.log(magician_1.charAt(0).toUpperCase() + magician_1.slice(1));
+    }
+}
+var magician = ["alisha", "tania", "ifra"];
+show_magicians(magician);
---
+++ app.ts
@@ -0,0 +1,8 @@
+// Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+function show_magicians(magicians: string[]): void {
+    for (const magician of magicians) {
+        console.log(magician.charAt(0).toUpperCase() +magician.slice(1));
+    }
+}
+const magician: string[] = ["alisha","tania","ifra"];
+show_magicians(magician)
